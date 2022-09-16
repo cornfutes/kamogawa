bf8b3290af87699aea07654b6a4f37ca94332286
Louis V <diceduckmonk@gmail.com>
2022-09-16 17:44:56 +0900

    Rearrange

--- cache/gcecache/gcecache.go
+++ cache/gcecache/gcecache.go
@@ -9,40 +9,6 @@ import (
 	"gorm.io/gorm"
 )
 
-func ReadInstancesCache(db *gorm.DB, projectId string) (*gcetypes.GCEAggregatedInstances, error) {
-	var gceInstanceDBs []gcetypes.GCEInstanceDB
-	result := db.Where("project_id = ?", projectId).Find(&gceInstanceDBs)
-	if result.Error != nil {
-		fmt.Printf("Query failed\n")
-		return nil, fmt.Errorf("Query failed")
-	}
-
-	if len(gceInstanceDBs) == 0 {
-		fmt.Printf("Cache miss\n")
-		return nil, fmt.Errorf("Cache miss")
-	}
-	fmt.Printf("Cache hit\n")
-
-	gceAggregatedInstances := gcetypes.GCEInstanceDBToGCEAggregatedInstances(gceInstanceDBs)
-	return &gceAggregatedInstances, nil
-}
-
-func WriteInstancesCache(db *gorm.DB, user types.User, projectId string, resp *gcetypes.GCEAggregatedInstances) {
-	if resp == nil {
-		panic("Unexpected list of instances")
-	}
-	if len(resp.Zones) == 0 {
-		return
-	}
-
-	gceInstanceDBs := gcetypes.GCEAggregatedInstancesToGCEInstanceDB(user, projectId, resp)
-
-	for _, gceInstanceDB := range gceInstanceDBs {
-		db.FirstOrCreate(&gceInstanceDB)
-	}
-
-}
-
 func ReadProjectsCache(db *gorm.DB, user types.User) (*gcetypes.ListProjectResponse, error) {
 	var projectDBs []gcetypes.ProjectDB
 
@@ -69,25 +35,6 @@ func ReadProjectsCache(db *gorm.DB, user types.User) (*gcetypes.ListProjectRespo
 	return &gcetypes.ListProjectResponse{Projects: projects}, nil
 }
 
-func WriteProjectsCache(db *gorm.DB, user types.User, resp *gcetypes.ListProjectResponse) {
-	if resp == nil {
-		panic("Unexpected list of projects")
-	}
-	if len(resp.Projects) == 0 {
-		return
-	}
-
-	projectDBs := make([]gcetypes.ProjectDB, 0, len(resp.Projects))
-	for _, v := range resp.Projects {
-		projectDBs = append(projectDBs, gcetypes.ProjectToProjectDB(&v, true))
-	}
-	for _, projectDB := range projectDBs {
-		db.FirstOrCreate(&projectDB)
-	}
-
-	WriteProjectsAuth(db, user, resp)
-}
-
 func ReadProjectsCache2(db *gorm.DB, user types.User) []gcetypes.ProjectDB {
 	var projectDBs []gcetypes.ProjectDB
 
@@ -106,6 +53,25 @@ func ReadProjectsCache2(db *gorm.DB, user types.User) []gcetypes.ProjectDB {
 	fmt.Printf("Cache hit\n")
 
 	return projectDBs
+}
+
+func WriteProjectsCache(db *gorm.DB, user types.User, resp *gcetypes.ListProjectResponse) {
+	if resp == nil {
+		panic("Unexpected list of projects")
+	}
+	if len(resp.Projects) == 0 {
+		return
+	}
+
+	projectDBs := make([]gcetypes.ProjectDB, 0, len(resp.Projects))
+	for _, v := range resp.Projects {
+		projectDBs = append(projectDBs, gcetypes.ProjectToProjectDB(&v, true))
+	}
+	for _, projectDB := range projectDBs {
+		db.FirstOrCreate(&projectDB)
+	}
+
+	WriteProjectsAuth(db, user, resp)
 }
 
 func WriteProjectsCache2(db *gorm.DB, user types.User, projects []gcetypes.ProjectDB) {
@@ -150,4 +116,38 @@ func WriteProjectsAuth2(db *gorm.DB, user types.User, projects []gcetypes.Projec
 	for _, v := range projectAuths {
 		db.FirstOrCreate(&v)
 	}
+}
+
+func ReadInstancesCache(db *gorm.DB, projectId string) (*gcetypes.GCEAggregatedInstances, error) {
+	var gceInstanceDBs []gcetypes.GCEInstanceDB
+	result := db.Where("project_id = ?", projectId).Find(&gceInstanceDBs)
+	if result.Error != nil {
+		fmt.Printf("Query failed\n")
+		return nil, fmt.Errorf("Query failed")
+	}
+
+	if len(gceInstanceDBs) == 0 {
+		fmt.Printf("Cache miss\n")
+		return nil, fmt.Errorf("Cache miss")
+	}
+	fmt.Printf("Cache hit\n")
+
+	gceAggregatedInstances := gcetypes.GCEInstanceDBToGCEAggregatedInstances(gceInstanceDBs)
+	return &gceAggregatedInstances, nil
+}
+
+func WriteInstancesCache(db *gorm.DB, user types.User, projectId string, resp *gcetypes.GCEAggregatedInstances) {
+	if resp == nil {
+		panic("Unexpected list of instances")
+	}
+	if len(resp.Zones) == 0 {
+		return
+	}
+
+	gceInstanceDBs := gcetypes.GCEAggregatedInstancesToGCEInstanceDB(user, projectId, resp)
+
+	for _, gceInstanceDB := range gceInstanceDBs {
+		db.FirstOrCreate(&gceInstanceDB)
+	}
+
 }

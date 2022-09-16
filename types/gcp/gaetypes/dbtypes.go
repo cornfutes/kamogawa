50a6f1a05a5ae7698d5fb0ec71f6ed19aefe45f5
Louis V <diceduckmonk@gmail.com>
2022-09-16 17:44:54 +0900

    cleanup: remove bad struct tags in dbtypes

--- types/gcp/gaetypes/dbtypes.go
+++ types/gcp/gaetypes/dbtypes.go
@@ -3,23 +3,23 @@ package gaetypes
 import "fmt"
 
 type GAEServiceDB struct {
-	Name      string `json:"name"`
-	Id        string `json:"id,gorm:"primaryKey"`
-	ProjectId string `json:"id,gorm:"primaryKey"`
+	Name      string
+	Id        string `gorm:"primaryKey"`
+	ProjectId string `gorm:"primaryKey"`
 }
 
 type GAEVersionDB struct {
-	Name          string `json:"name"`
-	Id            string `json:"id",gorm:"primaryKey"`
-	ServingStatus string `json:"servingStatus"`
-	ServiceId     string `json:"id,gorm:"primaryKey"`
+	Name          string
+	Id            string `gorm:"primaryKey"`
+	ServingStatus string
+	ServiceId     string `gorm:"primaryKey"`
 }
 
 type GAEInstanceDB struct {
-	Name      string `json:"name"`
-	Id        string `json:"id,gorm:"primaryKey""`
-	VMName    string `json:"vmName"`
-	VersionId string `json:"id,gorm:"primaryKey"`
+	Name      string
+	Id        string `gorm:"primaryKey"`
+	VMName    string
+	VersionId string `gorm:"primaryKey"`
 }
 
 func (in *GAEServiceDB) ToSearchString() string {
